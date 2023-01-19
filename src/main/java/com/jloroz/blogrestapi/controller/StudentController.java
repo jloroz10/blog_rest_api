b64dfdc38d26ab7b8186736302f6561c83d76a49
luis orozco <v-jarizaga@expediagroup.com>
2023-01-18 18:52:35 -0600

    Creating endpoint with @PostMapping and @RequestBody

--- src/main/java/com/jloroz/blogrestapi/controller/StudentController.java
+++ src/main/java/com/jloroz/blogrestapi/controller/StudentController.java
@@ -1,9 +1,14 @@
 package com.jloroz.blogrestapi.controller;
 
 import com.jloroz.blogrestapi.bean.Student;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +16,8 @@ import java.util.List;
 
 @RestController
 public class StudentController {
+
+    Logger log = LoggerFactory.getLogger(StudentController.class);
 
     @GetMapping("/student")
     public ResponseEntity getStudent(){
@@ -44,6 +51,12 @@ public class StudentController {
                                                        @RequestParam("last-name") String lastName){
 
         return ResponseEntity.ok(new Student(studentId,firstName,lastName));
+    }
+
+    @PostMapping("/student/create")
+    public ResponseEntity studentCreate(@RequestBody Student student){
+        log.info(student.toString());
+        return new ResponseEntity(student, HttpStatus.CREATED);
     }
 
 }
