b214e75af1773e5c85c95b3437a9ecc5a2c519e4
luis orozco <v-jarizaga@expediagroup.com>
2023-01-27 10:19:22 -0600

    creating the post entity class

---
+++ src/main/java/com/jloroz/blogrestapi/model/Post.java
@@ -0,0 +1,35 @@
+package com.jloroz.blogrestapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(
+        name="posts",
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})}
+)
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    @Column(name="title", nullable = false)
+    private String title;
+    @Column(name="description", nullable = false)
+    private String description;
+    @Column(name="content", nullable = false)
+    private String content;
+}
