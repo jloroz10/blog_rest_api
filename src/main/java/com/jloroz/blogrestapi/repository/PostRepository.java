e8a5ce4a41d557f2ad70ed7dfbed08160d7094ee
luis orozco <v-jarizaga@expediagroup.com>
2023-01-27 11:59:01 -0600

    Creating JPA Repository -PostRepository

---
+++ src/main/java/com/jloroz/blogrestapi/repository/PostRepository.java
@@ -0,0 +1,8 @@
+package com.jloroz.blogrestapi.repository;
+
+import com.jloroz.blogrestapi.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post,Long> {
+    Post findByName(String name);
+}
